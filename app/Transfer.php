c695932d01b4fa22e38863e7570f8a1c72e670b4
Peñafiel Erick <penafiel.erick.martin@gmail.com>
2026-03-02 16:39:37 -0400

    Fix transfers company

--- app/Transfer.php
+++ app/Transfer.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class Transfer extends Model
 {
@@ -20,6 +22,7 @@ class Transfer extends Model
         "grand_total",
         "document",
         "note",
+        "company_id",
     ];
 
     public function fromWarehouse()
@@ -49,5 +52,23 @@ class Transfer extends Model
         return $this->belongsTo(Biller::class, 'biller_id');
     }
 
+    protected static function boot()
+    {
+        parent::boot();
 
+        $table = (new static)->getTable();
+        if (Schema::hasColumn($table, 'company_id')) {
+            static::addGlobalScope('company', function (Builder $builder) use ($table) {
+                if (auth()->check()) {
+                    $builder->where($table . '.company_id', auth()->user()->company_id);
+                }
+            });
+
+            static::creating(function ($model) {
+                if (auth()->check() && empty($model->company_id)) {
+                    $model->company_id = auth()->user()->company_id;
+                }
+            });
+        }
+    }
 }
