e23be4368018cdd0aabc9d43cfdcd552cf2aa506
GisulRE <48808343+GisulRE@users.noreply.github.com>
2026-08-23 21:52:52 -0400

    perf(core): asignar limite global de memoria de 1024M en AppServiceProvider para prevenir desbordamientos al deserializar cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,10 +33,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        /*if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
-        URL::forceScheme('https');
-        }**/
-        //setting language
+        @ini_set('memory_limit', '1024M');
         if (isset($_COOKIE['language'])) {
             \App::setLocale($_COOKIE['language']);
         } else {
